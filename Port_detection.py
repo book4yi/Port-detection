016300b07d738a2f424d9bc919e3522a457050c2
sws12345 <59994062+sws12345@users.noreply.github.com>
2020-05-03 13:59:24 +0800

    Update Port_detection.py

--- Port_detection.py
+++ Port_detection.py
@@ -3,186 +3,145 @@ import queue
 import nmap
 import datetime
 import threading
-import requests
-import re
 import json
 import os
-import sys
 import urllib3
-import argparse
-from selenium import webdriver
+import subprocess
+import sys
+import click
+
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-
-final_domains = []
+lock = threading.Lock()
+final_domains = set()
+insert = set()
 ports = []
+bad_ips = []
 
 
 class PortScan(threading.Thread):
     def __init__(self, queue):
         threading.Thread.__init__(self)
         self._queue = queue
-
+    
     def run(self):
         while not self._queue.empty():
             scan_ip = self._queue.get()
             try:
                 portscan(scan_ip)
-                Scan(scan_ip)
             except Exception as e:
                 print('run:', str(e))
                 pass
 
+
 # 调用masscan
-
-
 def portscan(scan_ip):
+    global count
     temp_ports = []  # 设定一个临时端口列表
-    os.system(
-        'masscan.exe ' +
-        scan_ip +
-        ' -p1-65535 -oJ masscan.json --rate 2000')
+    name = scan_ip + '.json'
+    command = 'masscan.exe ' + scan_ip +' -p21,22,23,25,53,67,68,80,81,82,83,84,85,86,87,88,89,110,139,143,161,300,' \
+        '389,443,445,465,512,513,514,591,593,832,837,873,888,901,981,993,1010,1080,1100,1241,1311,1352,1433,1434,' \
+        '1521,1527,1582,1583,1723,1944,2049,2082,2082,2086,2087,2095,2096,2181,2222,2301,2375,2480,3000,3128,3306,' \
+        '3333,3389,4000,4001,4002,4100,4125,4243,4443,4444,4567,4711,4712,4848,4849,4993,5000,5104,5108,5432,5555,' \
+        '5632,5800,5801,5802,5900,5901,5984,5985,5986,6082,6225,6346,6347,6379,6443,6480,6543,6789,6984,7000,7001,' \
+        '7002,7396,7474,7674,7675,7777,7778,8000,8001,8002,8003,8004,8005,8006,8008,8009,8010,8014,8042,8069,8075,' \
+        '8080,8081,8082,8083,8084,8085,8086,8087,8088,8089,8090,8091,8092,8093,8095,8016,8118,8123,8161,8172,8181,' \
+        '8200,8222,8243,8280,8281,8333,8384,8403,8443,8500,8530,8531,8800,8806,8834,8880,8881,8887,8888,8910,8983,' \
+        '8989,8990,8991,9000,9043,9060,9080,9090,9091,9200,9294,9295,9300,9443,9444,9800,9981,9988,9990,9999,10000,' \
+        '10880,11211,11371,12043,12046,12443,15672,16225,16080,18091,18092,20000,20720,24465,27017,27018,28017,28080,' \
+        '30821,43110,50070,61600 -oJ ' + name + ' --rate 100'
+    child = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+    child.wait()  # 等待任务完成
     # 提取json文件中的端口
-    with open('masscan.json', 'r') as f:
-        for line in f:
-            if line.startswith('{ '):
-                temp = json.loads(line[:-2])
-                temp1 = temp["ports"][0]
-                temp_ports.append(str(temp1["port"]))
-    if len(temp_ports) > 50:
+    if os.path.exists(name):
+        with open(name, 'r') as f:
+            for line in f:
+                if line.startswith('{ '):
+                    temp = json.loads(line[:-2])
+                    temp1 = temp["ports"][0]
+                    temp_ports.append(str(temp1["port"]))
+    else:
+        print('文件不存在')
+        sys.exit()
+    if len(temp_ports) > 30:
+        count += 1
+        print(scan_ip + ' 疑似存在waf')
+        bad_ips.append(scan_ip)
         temp_ports.clear()  # 如果端口数量大于50，说明可能存在防火墙，属于误报，清空列表
     else:
-        ports.extend(temp_ports)  # 小于50则放到总端口列表里
+        ports.append(temp_ports)  # 小于50则放到总端口列表里
+        ips[scan_ip] = temp_ports
+        if os.path.exists(name):
+            os.remove(name)
+            print('file detele')
+        if ips.get(scan_ip):
+            Scan(scan_ip)
 
-
-# 获取网站的web应用程序名和网站标题信息
-def Title(scan_url_port, service_name):
-    try:
-        r = requests.get(scan_url_port, timeout=10, verify=False)
-        if len(r.apparent_encoding):
-            r.encoding = r.apparent_encoding
-        server = ''
-        language = ''
-        headers = str(r.headers)
-        if headers.find('Server') + 1:
-            server = r.headers['Server']
-        if headers.find('X-Powered-By') + 1:
-            language = r.headers['X-Powered-By']
-        title = re.findall(r'(?<=\<title\>)(?:.|\n)+?(?=\<)', r.text, re.S)
-        title = str(title).strip('[]').strip('\'')
-        if not title:
-            title = re.search('>.+</title>', r.text)
-            if title:
-                title = title.group()
-                title = title.strip('>').strip('</title')
-        if not title:
-            title = get_title(scan_url_port)
-            final_domains.append(
-                scan_url_port +
-                '    ' +
-                service_name +
-                '    ' +
-                language +
-                '    ' +
-                server +
-                '    ' +
-                title)
-        else:
-            final_domains.append(
-                scan_url_port +
-                '    ' +
-                service_name +
-                '    ' +
-                language +
-                '    ' +
-                server +
-                '    ' +
-                title)
-    except Exception as e:
-        print('Title:', str(e))
-        pass
 
 # 调用nmap识别服务
-
-
 def Scan(scan_ip):
+    global count
+    open_ports_list = ips[scan_ip]
+    open_ports = ",".join(open_ports_list)
     nm = nmap.PortScanner()
+    lock.acquire()
+    click.secho(f'[*] 开始nmap扫描 ip: {scan_ip} => 端口: {open_ports}', fg='red')
+    count += 1
+    print('当前是第', count, '个目标')
+    lock.release()
     try:
-        for port in ports:
-            ret = nm.scan(scan_ip, port, arguments='-Pn,-sS')
-            service_name = ret['scan'][scan_ip]['tcp'][int(port)]['name']
-            print(
-                '[*]主机 ' +
-                scan_ip +
-                ' 的 ' +
-                str(port) +
-                ' 端口服务为：' +
-                service_name)
-            if service_name in ['https']:
-                scan_url_port = 'https://' + scan_ip + ':' + str(port)
-                Title(scan_url_port, service_name)
-            elif service_name in ['http']:
-                scan_url_port = 'http://' + scan_ip + ':' + str(port)
-                Title(scan_url_port, service_name)
-            else:
-                final_domains.append(
-                    scan_ip + ':' + str(port) + '    ' + service_name)
+        ret = nm.scan(scan_ip, open_ports, arguments=nmap_arguments)
+        try:
+            output_item = ret['scan'][scan_ip]['tcp']
+        except Exception:
+            pass
+        else:   # try语句无异常时执行else语句
+            for port, port_info in output_item.items():  # 返回可遍历的(键, 值) 元组数组
+                save_item = f"[+] {scan_ip} {port} {port_info['name']} {port_info['product']} {port_info['version']}"
+                insert.add(scan_ip + '\t' + str(port) + '\t' + port_info['name'] + '\t' + port_info['product'] + ' '
+                           + port_info['version'] + '\n')
+                lock.acquire()
+                print(save_item)
+                lock.release()
+            fw = open('ports3.txt', 'w+', encoding='utf-8')
+            fw.writelines(insert)
+            fw.close()
     except Exception as e:
-        print('Scan:', str(e))
+        print(str(e))
         pass
-
-
-def get_title(test_url):
-    chrome_options = webdriver.ChromeOptions()
-    # 使用headless无界面浏览器模式
-    chrome_options.add_argument('--headless')  # 增加无界面选项
-    chrome_options.add_argument('--disable-gpu')  # 如果不加这个选项，有时定位会出现问题
-    browser = webdriver.Chrome(options=chrome_options)
-    browser.get(test_url)
-    title = browser.title
-    browser.quit()
-    return title
-
-
-def parser_args():
-    parser = argparse.ArgumentParser(usage='python Port_detection.py --target [urls file]')
-    parser.add_argument('-t', '--target', dest='target', help='The destination of file path', required=True)
-    if len(sys.argv) == 1:
-        sys.argv.append('-h')
-    args = parser.parse_args()
-    return args
 
 
 def main():
     que = queue.Queue()
     try:
-        f = open(url_path, 'r')
+        f = open(r'ips.txt', 'r')
         for line in f.readlines():
             final_ip = line.strip()
             que.put(final_ip)
+        f.close()
         threads = []
-        thread_count = 30
+        thread_count = 6
         for i in range(thread_count):
             threads.append(PortScan(que))
         for t in threads:
             t.start()
         for t in threads:
             t.join()
-        f.close()
     except Exception as e:
         print('Main:', e)
         pass
+    spend_time = (datetime.datetime.now() - start_time).seconds
+    print("疑似存在waf的IP：")
+    print(bad_ips)
+    print('程序共运行了： ' + str(spend_time) + '秒')
 
 
 if __name__ == '__main__':
+    ips = {}
+    index = 1
+    count = 0
     start_time = datetime.datetime.now()
-    argv = parser_args()
-    url_path = argv.target
-    path = r"D:\Security Tools\masscan1.0.4\x64"              #注意更改路径
+    path = r"D:\Security-Tools\masscan1.0.4\x64"    # masscan所在路径，可自行修改
     os.chdir(path)
+    nmap_arguments = "-sV -Pn"
     main()
-    for url in final_domains:
-        with open(r'scan_url_port.txt', 'a') as ff:
-            ff.write(url + '\n')
-    spend_time = (datetime.datetime.now() - start_time).seconds
-    print('程序共运行了： ' + str(spend_time) + '秒')
